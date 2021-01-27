9efa2cd62d8147a4ca9084e1c7509234075a1739
Andranik Tsaturyan <andtsatur@Andraniks-MacBook-Pro.local>
2021-01-27 18:08:42 +0400

    linerchart center

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import './App.css'
-import Header from './components/Header'
+import "./App.css";
+import Header from "./components/Header";
+import LinerChart from "./components/charts/linerChart";
+import PieChart from "./components/charts/pieChart";
+import Cart from "./components/card/Cart";
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-        </div>
-    )
+	return (
+		<div className="App">
+			<Header />
+
+			<div className="linerChart">
+				<div className="cart-container">
+					<Cart />
+					<Cart />
+					<Cart />
+				</div>
+				<div className="linerChart-container">
+					<LinerChart />
+				</div>
+			</div>
+
+			<div className="pieChart">
+				<PieChart />
+				<PieChart />
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
---
+++ src/components/card/Cart.js
@@ -0,0 +1,15 @@
+export default function App(props) {
+	return (
+		<div className="cart">
+			<svg width="30" height="30" xmlns="http://www.w3.org/2000/svg">
+				<image
+					href="https://www.flaticon.com/svg/vstatic/svg/3135/3135706.svg?token=exp=1611754089~hmac=d23a8826be72afe476f901469d43f907"
+					height="30"
+					width="30"
+				/>
+			</svg>
+			<span>15000</span>
+			<p>cost</p>
+		</div>
+	);
+}
