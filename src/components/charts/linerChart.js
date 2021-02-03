e479eb7ad28c2d960ada09f6cb2e3fbb9514ec58
Arthur <hayart98@gmail.com>
2021-02-03 12:53:12 +0400

    added bar charts in cards

--- src/components/charts/linerChart.js
+++ src/components/charts/linerChart.js
@@ -2,33 +2,123 @@ import { AreaChart, XAxis, YAxis, Tooltip, CartesianGrid, Area } from 'recharts'
 const data = [
     {
         name: 'March',
-        In: 4000,
-        Out: 2400,
+        In: 1200,
+        Out: 1300,
     },
     {
         name: 'April',
-        In: 2000,
-        Out: 3500,
+        In: 6000,
+        Out: 3541,
     },
     {
         name: 'May',
-        In: 3000,
+        In: 2000,
         Out: 1500,
     },
     {
         name: 'Jun',
-        In: 5000,
-        Out: 2400,
+        In: 2000,
+        Out: 5400,
     },
     {
         name: 'July',
-        In: 5000,
-        Out: 7400,
+        In: 3800,
+        Out: 1400,
     },
     {
         name: 'Ougust',
-        In: 7000,
+        In: 2500,
         Out: 3000,
+    },
+    {
+        name: 'July',
+        In: 3800,
+        Out: 1400,
+    },
+    {
+        name: 'Ougust',
+        In: 2500,
+        Out: 3000,
+    },
+    {
+        name: 'March',
+        In: 1200,
+        Out: 1300,
+    },
+    {
+        name: 'April',
+        In: 6000,
+        Out: 3541,
+    },
+    {
+        name: 'May',
+        In: 2000,
+        Out: 1500,
+    },
+    {
+        name: 'Jun',
+        In: 2000,
+        Out: 5400,
+    },
+    {
+        name: 'July',
+        In: 3800,
+        Out: 1400,
+    },
+    {
+        name: 'Ougust',
+        In: 2500,
+        Out: 3000,
+    },
+    {
+        name: 'July',
+        In: 3800,
+        Out: 1400,
+    },
+    {
+        name: 'Ougust',
+        In: 5500,
+        Out: 6000,
+    },
+    {
+        name: 'March',
+        In: 1200,
+        Out: 1300,
+    },
+    {
+        name: 'April',
+        In: 6000,
+        Out: 3541,
+    },
+    {
+        name: 'May',
+        In: 2600,
+        Out: 500,
+    },
+    {
+        name: 'Jun',
+        In: 3000,
+        Out: 1000,
+    },
+    {
+        name: 'July',
+        In: 800,
+        Out: 400,
+    },
+    {
+        name: 'Ougust',
+        In: 6500,
+        Out: 1000,
+    },
+    {
+        name: 'July',
+        In: 2800,
+        Out: 4400,
+    },
+    {
+        name: 'Ougust',
+        In: 1500,
+        Out: 2000,
     },
 ]
 
