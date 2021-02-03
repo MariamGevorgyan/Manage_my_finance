d77d091f75d73dd57918b9e6c353a01dcb364b26
Arthur <hayart98@gmail.com>
2021-02-03 12:08:24 +0400

    any css changes

--- src/components/card/Cart.js
+++ src/components/card/Cart.js
@@ -1,15 +1,13 @@
 export default function App(props) {
-	return (
-		<div className="cart">
-			<svg width="30" height="30" xmlns="http://www.w3.org/2000/svg">
-				<image
-					href="https://www.flaticon.com/svg/vstatic/svg/3135/3135706.svg?token=exp=1611754089~hmac=d23a8826be72afe476f901469d43f907"
-					height="30"
-					width="30"
-				/>
-			</svg>
-			<span>15000</span>
-			<p>cost</p>
-		</div>
-	);
+    return (
+        <div className="cart">
+            <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg">
+                <image href={props.icon} height="30" width="30" />
+            </svg>
+            <div className="card_type_money">
+                <span className="card_type">{props.type}</span>
+                <span className="card_money">{props.money}</span>
+            </div>
+        </div>
+    )
 }
